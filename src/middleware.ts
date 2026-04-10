9f28f928f509dd21f8c1db6284a2daa6478b7152
Maycon Bentes <mayconabentes@gmail.com>
2026-04-10 17:17:07 -0400

    fix: revert middleware rename to fix 500 internal server error

--- src/middleware.ts
+++ src/middleware.ts
@@ -39,7 +39,7 @@ export const updateSession = async (request: NextRequest) => {
   return supabaseResponse
 };
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   return await updateSession(request)
 }
 
